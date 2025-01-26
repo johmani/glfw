131ccfe759f5c6af5919dfe5fe4fbbfc675e2d95
Mohamd Johmani <mohamdaljohmani@gmail.com>
2025-01-26 10:04:58 +0300

    update premake

--- premake5.lua
+++ premake5.lua
@@ -4,8 +4,8 @@ project "GLFW"
 	staticruntime "off"
 	warnings "off"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir (libOutputDir)
+    objdir (IntermediatesOutputDir)
 
 	files
 	{
